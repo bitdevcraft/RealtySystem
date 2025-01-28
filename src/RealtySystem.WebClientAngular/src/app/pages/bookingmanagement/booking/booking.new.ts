959f189ba0f970c70381adfabd8342af90df5205
The Bit Dev <ryanvincent.capio@outlook.com>
2025-01-28 16:40:40 +0400

    Added filter field for room.

--- src/RealtySystem.WebClientAngular/src/app/pages/bookingmanagement/booking/booking.new.ts
+++ src/RealtySystem.WebClientAngular/src/app/pages/bookingmanagement/booking/booking.new.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { Button, ButtonModule } from 'primeng/button';
 import { Card, CardModule } from 'primeng/card';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -11,25 +11,62 @@ import { TagModule } from 'primeng/tag';
 import { PrefixSuffixPipe } from '../../../utils/pipe/prefixsuffix.pipe';
 import { Select } from 'primeng/select';
 import { MultiSelectChangeEvent, MultiSelectModule } from 'primeng/multiselect';
-import { FilterCriterion } from '../../service/generic.service';
+import { ComparisonOperator, FilterCriterion, MatchType } from '../../service/generic.service';
 import { Fluid, FluidModule } from 'primeng/fluid';
 import { InputText } from 'primeng/inputtext';
+import { AutoComplete, AutoCompleteCompleteEvent } from 'primeng/autocomplete';
+import { ProjectService } from '../../service/project.service';
+import { CommunityService } from '../../service/community.service';
+import { Popover } from 'primeng/popover';
+import { TableModule } from 'primeng/table';
+import { InputNumber } from 'primeng/inputnumber';
 
 @Component({
     selector: 'booking-new',
-    imports: [ButtonModule, CardModule, ReactiveFormsModule, DataViewModule, FormsModule, SelectButtonModule, NgClass, TagModule, NgForOf, PrefixSuffixPipe, DecimalPipe, MultiSelectModule, FluidModule, InputText, Select],
+    imports: [
+        ButtonModule,
+        CardModule,
+        ReactiveFormsModule,
+        DataViewModule,
+        FormsModule,
+        SelectButtonModule,
+        NgClass,
+        TagModule,
+        NgForOf,
+        PrefixSuffixPipe,
+        DecimalPipe,
+        MultiSelectModule,
+        FluidModule,
+        Select,
+        AutoComplete,
+        Popover,
+        TableModule,
+        InputNumber
+    ],
     template: `
-        <div class="card">
+        <div class="card min-w-96">
             <p-dataview [value]="records()" [layout]="layout" [lazyLoadOnInit]="true" [rows]="12" [paginator]="true" [rowsPerPageOptions]="[12, 24, 36, 48]">
                 <ng-template #header>
-                    <div class="flex justify-between">
-                        <div class="font-semibold text-xl">Available Properties</div>
-                        <p-select-button [(ngModel)]="layout" [options]="options" [allowEmpty]="false">
-                            <ng-template #item let-option>
-                                <i class="pi " [ngClass]="{ 'pi-bars': option === 'list', 'pi-table': option === 'grid' }"></i>
-                            </ng-template>
-                        </p-select-button>
+                    <div class="flex flex-col md:flex-row justify-between w-full items-center mb-4">
+                        <div class="font-semibold text-xl w-full">Available Properties</div>
+
+                        <div class="flex gap-2 md:flex-row flex-col w-full justify-end items-center">
+                            <p-fluid>
+                                <div class="flex flex-col md:flex-row gap-6">
+                                    <div class="flex flex-wrap flex-row gap-2 w-full items-center justify-end">
+                                        <label>Sort</label>
+                                        <p-select styleClass="w-full md:w-80" [options]="sortOption" optionLabel="name" optionValue="code" placeholder="Sort"></p-select>
+                                    </div>
+                                </div>
+                            </p-fluid>
+                            <p-select-button [(ngModel)]="layout" [options]="options" [allowEmpty]="false">
+                                <ng-template #item let-option>
+                                    <i class="pi " [ngClass]="{ 'pi-bars': option === 'list', 'pi-table': option === 'grid' }"></i>
+                                </ng-template>
+                            </p-select-button>
+                        </div>
                     </div>
+
                     <p-fluid>
                         <div class="flex flex-col md:flex-row gap-6">
                             <div class="flex flex-col gap-2 w-full">
@@ -38,19 +75,29 @@ import { InputText } from 'primeng/inputtext';
                             </div>
                             <div class="flex flex-col gap-2 w-full">
                                 <label for="project">Project</label>
-                                <input pInputText type="text" />
+                                <p-autoComplete multiple optionLabel="name" [suggestions]="autoFilteredValue" (completeMethod)="filterProjects($event)"></p-autoComplete>
                             </div>
                             <div class="flex flex-col gap-2 w-full">
                                 <label for="community">Community</label>
-                                <input pInputText type="text" />
+                                <p-autoComplete multiple optionLabel="name" [suggestions]="autoFilteredValue" (completeMethod)="filterCommunity($event)"></p-autoComplete>
                             </div>
                             <div class="flex flex-col gap-2 w-full">
                                 <label for="room">Room</label>
-                                <input pInputText type="text" />
-                            </div>
-                            <div class="flex flex-col gap-2 w-full">
-                                <label for="Sort">Sort</label>
-                                <input pInputText type="text" />
+                                <div>
+                                    <p-button [outlined]="true" severity="secondary" label="Room" (click)="op.toggle($event)"></p-button>
+                                    <p-popover #op>
+                                        <div class="flex flex-col gap-4 w-[12rem] ">
+                                            <p-select [(ngModel)]="matchValue" [options]="matchTypeOption" optionLabel="name" optionValue="code" placeholder="Select match type" fluid></p-select>
+                                            <p-select [(ngModel)]="numberOperatorValue" [options]="numberOperator" optionLabel="name" optionValue="code" placeholder="operator" fluid></p-select>
+                                            <p-inputNumber [(ngModel)]="numberValue" fluid></p-inputNumber>
+                                            <p-button size="small" label="Add rule" [text]="true" fluid icon="pi pi-plus"></p-button>
+                                            <div class="flex flex-row justify-between">
+                                                <p-button size="small" label="Clear" [outlined]="true" fluid icon="pi pi-filter-slash"></p-button>
+                                                <p-button size="small" label="Apply" fluid (onClick)="addRoomFilter($event)"></p-button>
+                                            </div>
+                                        </div>
+                                    </p-popover>
+                                </div>
                             </div>
                         </div>
                     </p-fluid>
@@ -92,9 +139,11 @@ import { InputText } from 'primeng/inputtext';
             </p-dataview>
         </div>
     `,
-    providers: [PropertyService, ProductService]
+    providers: [PropertyService, ProductService, ProjectService, CommunityService]
 })
 export class BookingNew {
+    @ViewChild('op') op!: Popover;
+
     layout: 'grid' | 'list' = 'grid';
 
     options = ['list', 'grid'];
@@ -109,6 +158,18 @@ export class BookingNew {
 
     selectedTypes: any[] = [];
 
+    autoFilteredValue: any[] = [];
+
+    roomsFilter: any[] = [];
+
+    matchTypeOption: any[] = [];
+
+    matchValue: MatchType = 'all';
+
+    numberOperator: any[] = [];
+
+    numberOperatorValue: ComparisonOperator = 'eq';
+
     typeFilter: FilterCriterion<Property> = {
         field: 'type',
         filters: [
@@ -120,11 +181,15 @@ export class BookingNew {
         fieldMatchType: 'all'
     };
 
+    numberValue!: number;
+
     filterCriteria: FilterCriterion<Property>[] = [this.typeFilter];
 
     constructor(
         private propertyService: PropertyService,
-        private productService: ProductService
+        private productService: ProductService,
+        private projectService: ProjectService,
+        private communityService: CommunityService
     ) {}
 
     ngOnInit() {
@@ -134,6 +199,25 @@ export class BookingNew {
             { name: 'Unit', code: 'Unit' },
             { name: 'Villa', code: 'Villa' },
             { name: 'Plot', code: 'Plot' }
+        ];
+
+        this.sortOption = [
+            { name: 'Default', code: 'Default' },
+            { name: 'Price lower to higher', code: 'Price lower to higher' },
+            { name: 'Price higher to lower', code: 'Price higher to lower' }
+        ];
+
+        this.matchTypeOption = [
+            { name: 'Match Any', code: 'any' },
+            { name: 'Match All', code: 'all' }
+        ];
+        this.numberOperator = [
+            { name: 'Equals', code: 'eq' },
+            { name: 'Not equals', code: 'neq' },
+            { name: 'Less than', code: 'lt' },
+            { name: 'Less than or equal to', code: 'lte' },
+            { name: 'Greater than', code: 'gt' },
+            { name: 'Greater than or equal to', code: 'gte' }
         ];
     }
 
@@ -169,5 +253,46 @@ export class BookingNew {
         this.propertyService.getFilteredProperty(this.filterCriteria, 'any').then((result) => {
             this.records.set(result);
         });
+    }
+
+    filterProjects(event: AutoCompleteCompleteEvent) {
+        const query = event.query;
+        this.projectService.getProjectsByName(query).then((data) => (this.autoFilteredValue = data));
+    }
+
+    filterCommunity(event: AutoCompleteCompleteEvent) {
+        const query = event.query;
+        this.communityService.getCommunitiesByName(query).then((data) => (this.autoFilteredValue = data));
+    }
+
+    toggle(event: any) {
+        this.op.toggle(event);
+    }
+
+    addRoomFilter(event: any) {
+        this.toggle(event);
+
+        const filter = this.filterCriteria.filter((criteria) => criteria.field === 'rooms');
+        const roomFilter: FilterCriterion<Property> = {
+            field: 'rooms',
+            filters: [
+                {
+                    operator: this.numberOperatorValue,
+                    value: this.numberValue
+                }
+            ],
+            fieldMatchType: this.matchValue
+        };
+
+        if (filter.length > 0) {
+            filter[0].filters.push({
+                operator: this.numberOperatorValue,
+                value: this.numberValue
+            });
+        } else {
+            this.filterCriteria.push(roomFilter);
+        }
+
+        console.log(this.filterCriteria);
     }
 }
